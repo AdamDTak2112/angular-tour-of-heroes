f33a96f4da48dbe9c7d3b55b6a3d99b912ad7a27
Adam Tak <adam.tak2112@gmail.com>
2021-01-31 18:47:13 -0700

    add messages component + hero service functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,6 +24,6 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.HeroService.getHeroes();
+    this.HeroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
